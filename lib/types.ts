aeaba111c43c66a8b4eac92456e589c88cbf0266
Levy van der Valk <levy@yaacomm.com>
2024-06-05 10:32:07 +0200

    chore: cleaned up types

--- lib/types.ts
+++ lib/types.ts
@@ -3,31 +3,15 @@
  */
 export interface IndentWrappedLinesOptions {
 	/**
-	 * The initial indent for the wrapped lines
-	 * 
+	 * The initial indent for the wrapped lines  
 	 * This indent is added before calculating the rest of the indent
-	 * 
-	 * @example
-	 * // initialIndent: 4
-	 * 
-	 * `this line would indent the wrapped lines
-	 *     by 4 times the initialIndentType
-	 *     and so forth`
-	 * 
 	 * @default 0
 	 */
 	initialIndent: number;
 
 	/**
-	 * Determines the type of indent to use for the initial indent
-	 * 
-	 * @example
-	 * // initialIndentType: "space"
-	 * 
-	 * `this line would indent the wrapped lines
-	 *     by 4 spaces
-	 *     and so forth`
-	 * 
+	 * Determines the type of indent to use for the initial indent  
+	 * This indent is added before calculating the rest of the indent
 	 * @default "space"
 	 */
 	initialIndentType: "space" | "tab" | "indentUnit";
